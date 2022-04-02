68a63aee72df08e2aa06a5a920c73396f65ad61c
Webdgepl <cholewski185@gmail.com>
2022-04-02 20:04:34 +0200

    Add FAQ section

--- wp-content/themes/cut2code/front-page.php
+++ wp-content/themes/cut2code/front-page.php
@@ -67,15 +67,34 @@
     </div>
 
 </section>
-<!-- 
+
 <section class="faq">
 
-    <h2 class="faq__title">Have any Question?</h2>
-    <p class="faq__subtitle">Cras accumsan pretium dignissim. Curabitur a orci lorem. Phasellus tempor dolor vel odio efficitur ac sollicitudin.</p>
-    <div class="faq__accordion">
+    <div class="faq__left">
+        <img class="faq__questionMark" src="<?php echo get_template_directory_uri(); ?>/img/icons/questionMark.png" alt="Question Mark">
 
+        <h2 class="faq__title">Have any Question?</h2>
+        <p class="faq__subtitle">Cras accumsan pretium dignissim. Curabitur a orci lorem. Phasellus tempor dolor vel odio efficitur ac sollicitudin.</p>
+
+        <div class="faq__accordion">
+            <div class="faq_accordion__item">
+                <h4 class="faq__accordion__question">How can I install the App?</h4>
+                <p class="faq__accordion__answer">Duis vestibulum elit vel neque pharetra vulputate. Quisque Proin scelerisque nisi urna. Duis rutrum non risus in imperdiet.</p>
+            </div>
+            <div class="faq__accordion__item">
+                <h4 class="faq__accordion__question">What is the Main Features of this App?</h4>
+                <p class="faq__accordion__answer"></p>
+            </div>
+            <div class="faq__accordion__item">
+                <h4 class="faq__accordion__question">Is there any Video Sessions?</h4>
+                <p class="faq__accordion__answer"></p>
+            </div>
+        </div>
     </div>
 
+    <div class="faq__right">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/faqBackground.png" alt="">
+    </div>
 
 </section>
 
@@ -88,7 +107,7 @@
 
 </section>
 
-<section class="contactUs">
+<!-- <section class="contactUs">
 
     <article class="contactUs__content">
         <h2 class="contactUs__title">Contact Us</h2>
