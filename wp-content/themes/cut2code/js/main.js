42d01b9459b7a3c33664e752ccd8dbcbe67f934f
Webdgepl <cholewski185@gmail.com>
2022-04-14 01:56:36 +0200

    Add intersection observer to statistics

--- wp-content/themes/cut2code/js/main.js
+++ wp-content/themes/cut2code/js/main.js
@@ -1,25 +1,46 @@
 $ = jQuery.noConflict(true);
 
-var speed = 20;
+/*--------- Intersection Observer for statistics ----------*/
 
-function incElementNumber(id) {
-	let element = $(id);
-	let endNumber = $(id).text();
-	incElementNumberRecursive(0, endNumber, element);
-}
+let statistics = document.querySelector(".statistics");
+let observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.intersectionRatio == 1) {
+				increaseNumbers();
+			}
+		});
+	},
+	{ threshold: 1 }
+);
+observer.observe(statistics);
 
-function incElementNumberRecursive(i, endNumber, element) {
-	if (i <= endNumber) {
-		element.text(i);
-		setTimeout(function () {
-			incElementNumberRecursive(i + 1, endNumber, element);
-		}, speed);
+/*---------- Increase numbers ------------*/
+
+function increaseNumbers() {
+	var speed = 150;
+
+	function incElementNumber(id) {
+		let element = $(id);
+		let endNumber = $(id).text();
+		incElementNumberRecursive(0, endNumber, element);
 	}
-}
 
-$(".nbr").each(function () {
-	incElementNumber(this);
-});
+	function incElementNumberRecursive(i, endNumber, element) {
+		if (i <= endNumber) {
+			element.text(i);
+			speed -= 2;
+			setTimeout(function () {
+				incElementNumberRecursive(i + 1, endNumber, element);
+			}, speed);
+		}
+	}
+
+	$(".nbr").each(function () {
+		incElementNumber(this);
+		observer.unobserve(statistics);
+	});
+}
 
 /*---------- ACCORDION ----------*/
 
