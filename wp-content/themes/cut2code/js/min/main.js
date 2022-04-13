e6a460cb03e32b11aea68164a03d5c80feee5232
Webdgepl <cholewski185@gmail.com>
2022-04-13 21:19:39 +0200

    Add increasing numbers and accordion

--- wp-content/themes/cut2code/js/min/main.js
+++ wp-content/themes/cut2code/js/min/main.js
@@ -1,1 +1,36 @@
-console.log("js ready");
+$ = jQuery.noConflict(true);
+
+var speed = 20;
+
+function incElementNumber(id) {
+	let element = $(id);
+	let endNumber = $(id).text();
+	incElementNumberRecursive(0, endNumber, element);
+}
+
+function incElementNumberRecursive(i, endNumber, element) {
+	if (i <= endNumber) {
+		element.text(i);
+		setTimeout(function () {
+			incElementNumberRecursive(i + 1, endNumber, element);
+		}, speed);
+	}
+}
+
+$(".nbr").each(function () {
+	incElementNumber(this);
+});
+
+/*---------- ACCORDION ----------*/
+
+$(".question").click(function () {
+	let answer = $(this).next();
+
+	if (answer.length > 0) {
+		if (answer.is(":visible")) {
+			answer.slideUp();
+		} else {
+			answer.slideDown();
+		}
+	}
+});
